6bc0dbda59078c818cee9aa8d0a884b71f02ac3e
julianvolpi <julianvolpi@gmail.com>
2026-07-24 14:21:44 -0300

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
 import app from "./app.js";
 import { prisma } from "./db/prisma.js";
 
+const shutdown = async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+};
+
 const port = Number(process.env.PORT) || 4000;
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
 
-process.on("SIGINT", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
-
-process.on("SIGTERM", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);

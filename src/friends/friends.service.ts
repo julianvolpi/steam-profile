93d9ba513d337dfb88ef51f2b37a1f7430f584f2
julianvolpi <julianvolpi@gmail.com>
2026-08-01 21:27:39 -0300

    adding friendsSince to friends response

--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -3,12 +3,17 @@ import { toUserProfile } from "../users/users.mapper.js";
 
 export const fetchFriends = async (steamId: string) => {
   const friends = (await getPlayerFriends(steamId)).friendslist.friends;
+  const friendsSince = new Map<string, Date>();
 
   const friendsWithProfilesResponse = await getPlayerSummaries(
-    friends.map((friend) => friend.steamid),
+    friends.map((friend) => {
+      friendsSince.set(friend.steamid, new Date(friend.friend_since * 1000));
+      return friend.steamid;
+    }),
   );
 
-  return friendsWithProfilesResponse.response.players.map((player) =>
-    toUserProfile(player),
-  );
+  return friendsWithProfilesResponse.response.players.map((player) => ({
+    ...toUserProfile(player),
+    friendSince: friendsSince.get(player.steamid),
+  }));
 };
